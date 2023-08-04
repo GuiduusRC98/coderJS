bcc1d6d861696f126e8e2cc0aad96fc5b05efbaa
Guido Pezzotta <pezzottaguido@gmail.com>
2023-08-04 18:45:56 -0300

    Luego de varios intento para que funcione correctamente para sacar intereses de la empresa donde trabajo

---
+++ app.js
@@ -0,0 +1,36 @@
+const calcularInteres = (dias, cuotas, importe) => {
+    let resultado = (2.5 / 30) * (30 - dias) + (2.5 * cuotas);
+    return (resultado * importe) / 100;
+  };
+  
+  let SolicitudFinanciacion = false;
+  let genero = "";
+  let edad = "";
+  let edadMujer = 60;
+  let edadHombre = 65;
+  
+  while (!SolicitudFinanciacion) {
+    genero = prompt("Ingrese si es hombre o mujer");
+    edad = Number(prompt("Ingrese su edad"));
+  
+    if (genero === "hombre" && edad < edadHombre) {
+      let dias = Number(prompt("Ingrese qué día es hoy"));
+      let cuotas = Number(prompt("Ingrese cantidad de cuotas"));
+      let importe = Number(prompt("Ingrese importe"));
+    
+      let resultadoFinal = calcularInteres(dias, cuotas, importe);
+    
+      alert("El interés es: " + resultadoFinal);
+    } else if (genero === "mujer" && edad < edadMujer) {
+        let dias = Number(prompt("Ingrese qué día es hoy"));
+        let cuotas = Number(prompt("Ingrese cantidad de cuotas"));
+        let importe = Number(prompt("Ingrese importe"));
+      
+        let resultadoFinal = calcularInteres(dias, cuotas, importe);
+      
+        alert("El interés es: " + resultadoFinal);
+    }else{
+        alert("No puedes realizar financiación.");
+    }
+  }
+  
