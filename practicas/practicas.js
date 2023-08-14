96f08f855098c00b9e2f2141d4b056cdd42c57e9
Guido Pezzotta <pezzottaguido@gmail.com>
2023-08-14 18:47:41 -0300

    practicas yt

---
+++ practicas/practicas.js
@@ -0,0 +1,21 @@
+const arrayDeObjetos = [
+    {
+        nombre: "Bruno",
+        edad: 23,
+    },
+    {
+        nombre: "Gina",
+        edad: 18,
+    },
+    {
+        nombre: "Guido",
+        edad: 24,
+    },
+];
+
+for (let i = 0; i < arrayDeObjetos.length; i++) {
+    console.log("===================")
+    console.log("Nombre: " + arrayDeObjetos[i].nombre);
+    console.log("Edad: " +arrayDeObjetos[i].edad);
+    
+}
---
+++ practicas/practicasdos.js
@@ -0,0 +1,8 @@
+const objetoConArray ={
+    nombre: "objeto con array",
+    arr: ["abeja", "banana", "camion"]
+};
+
+objetoConArray.arr.forEach((elemento) => {
+    console.log(elemento + " jaajaja")
+});
