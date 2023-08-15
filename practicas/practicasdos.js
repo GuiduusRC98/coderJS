3950f70de4986f836a5c469a4c845691fbaf9100
Guido Pezzotta <pezzottaguido@gmail.com>
2023-08-14 22:33:44 -0300

    clase 7

--- practicas/practicasdos.js
+++ practicas/practicasdos.js
@@ -1,3 +1,9 @@
+const numeros = [1,2,3,4,5,6]
+
+numeros.forEach( (num)=> {
+    console.log(num)
+} )
+
 const objetoConArray ={
     nombre: "objeto con array",
     arr: ["abeja", "banana", "camion"]
